5b4cae87dea71a0516005d64086c9bac17d3e079
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-10-02 16:04:08 -0300

    commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,75 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import styles from '../page.module.scss'
+import logoImg from '/public/logo.svg'
+
 export default function Signup(){
+
+  async function handleRegister(formData: FormData){
+    'use server'
+
+    const name = formData.get('name')
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    if( name === "" || email === "" || password === "" ){
+      return;
+    }
+  }
+
     return(
     <>
-    <main>
-        <h1>Pagina cadastro</h1>
-    </main>
+     <div className={styles.containerCenter}>
+      <Image 
+      src={logoImg}
+      alt='Logo da pizzaria'
+      />
+
+    <section className={styles.login}>
+
+        <h1>Criando sua conta</h1>
+
+      <form action={handleRegister}>
+
+      <input 
+        type='name'
+        required
+        name='name'
+        placeholder='Digite seu nome'
+        className={styles.input}
+        />
+
+        <input 
+        type='email'
+        required
+        name='email'
+        placeholder='Digite seu email'
+        className={styles.input}
+        />
+
+        <input 
+        type='password'
+        required
+        name='password'
+        placeholder='************'
+        className={styles.input}
+        />
+
+        <button type='submit' className={styles.button}>
+       Cadastrar
+        </button>
+
+      </form>
+
+    <Link href="/" className={styles.text}>
+    Já possui uma conta ? Faça o login
+    </Link>
+
+    </section>
+
+    </div>
     </>
     )
 }
